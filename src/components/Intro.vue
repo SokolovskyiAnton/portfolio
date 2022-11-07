f7bccfae192eabc52d6769dc801f574a53fd2a88
dev <dev@gmail.com>
2022-11-07 23:52:31 +0200

    create intro block

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,9 +1,18 @@
 <template>
   <section class="scene-intro" id="intro">
-    <div class="static-container">intro</div>
+    <div class="static-container">
+      <h1 class="title">
+        <span>sokolovskyi</span>
+        <span class="func">.is()</span>
+      </h1>
+      <TextBlock>
+        <p class="-purple">Anton Sokolovskyi</p>
+        <p class="-gray">Frontend developer</p>
+      </TextBlock>
+    </div>
   </section>
 </template>
 
-<script lang="ts" setup></script>
-
-<style lang="stylus" scoped></style>
+<script lang="ts" setup>
+import TextBlock from "@/components/TextBlock";
+</script>
---
+++ src/components/TextBlock.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="text-block">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="stylus">
+.text-block
+  max-width 42rem
+  line-height 1.5
+  color var(--text-color-light)
+  word-break break-word
+  hyphens none
+  font-size 1.2rem
+  @media screen and (max-width: 568px)
+    font-size 1rem
+
+  h3
+    margin-bottom 1rem
+
+  p
+    margin-bottom 1rem
+    &:last-child
+      margin-bottom 0
+  .-big
+    display block
+    font-size 4rem
+    font-weight 700
+    word-spacing -0.1em
+    letter-spacing -0.05em
+    width 70vw
+    line-height 1.2
+
+  .-purple
+    color var(--text-color-purple)
+
+  .-gray
+    color var(--text-color-gray)
+
+  .-comment
+    color var(--text-color-comment)
+
+  .-red
+    color var(--text-color-red)
+
+  .-green
+    color var(--text-color-green)
+
+  .-full
+    display block
+    margin-right -25vw
+    padding-right 1rem
+
+  i
+    font-style italic
+
+  b,
+  strong
+    font-weight 700
+
+  a
+    color var(--text-color-light)
+    text-decoration:underline
+    transition all 400ms ease-out
+    &:hover
+      color var(--text-color-purple)
+
+  .subtitle
+    color var(--text-color-purple)
+    font-size 4rem
+    line-height 1
+    margin 4rem -25vw 2rem 0
+    padding-right 1rem
+    word-spacing -1rem
+    word-break break-word
+    hyphens auto
+
+  ul
+    margin-bottom 1rem
+    &:last-child
+      margin-bottom 0
+
+  li
+    list-style-position outside
+    margin 0 0 0.5rem 1rem
+    &:last-child
+      margin-bottom 0
+</style>
